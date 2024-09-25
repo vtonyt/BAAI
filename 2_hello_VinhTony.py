71ef73ca7f36d9eb0ff64206831efe106300c5f0
Vinh Tony <vinhtony@outlook.com>
2024-09-25 17:01:06 +0800

    Add something

---
+++ 2_hello_VinhTony.py
@@ -0,0 +1,11 @@
+#
+# Vinh Tony
+# Print Hello Vinh Tony!
+#
+
+# 1. Input
+
+# 2. Process
+
+# 3. Output
+print('Hello Vinh Tony!')
---
+++ 3_hello_VinhTonz_input.py
@@ -0,0 +1,12 @@
+#
+# Vinh Tony
+# Take name as an input then print Hello name
+#
+
+# 1. Input
+n= input('Name: ')
+
+# 2. Process
+
+# 3. Output
+print(f'Hello {n}')
